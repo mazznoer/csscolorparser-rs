4f2a62d5bbd3616b30f591090d9378b024eb1e5a
Nor Khasyatillah <mazznoer@ymail.com>
2021-01-09 20:18:32 +0700

    move Color test to its own file

---
+++ tests/color.rs
@@ -0,0 +1,86 @@
+use csscolorparser::Color;
+
+#[test]
+fn basic() {
+    let c = Color::from_rgb(1., 0., 0.);
+    assert_eq!(c.rgba(), (1., 0., 0., 1.));
+    assert_eq!(c.rgba_u8(), (255, 0, 0, 255));
+    assert_eq!(c.to_hex_string(), "#ff0000");
+    assert_eq!(c.to_rgb_string(), "rgb(255,0,0)");
+    assert_eq!(c.to_string(), "RGBA(1,0,0,1)");
+    assert_eq!(c.to_hsva(), (0., 1., 1., 1.));
+    assert_eq!(c.to_hsla(), (0., 1., 0.5, 1.));
+    assert_eq!(c.to_hwba(), (0., 0., 0., 1.));
+    assert_eq!(c.red(), 1.);
+    assert_eq!(c.green(), 0.);
+    assert_eq!(c.blue(), 0.);
+    assert_eq!(c.alpha(), 1.);
+
+    let c = Color::from_rgba(1., 0., 0., 0.5);
+    assert_eq!(c.rgba(), (1., 0., 0., 0.5));
+    assert_eq!(c.rgba_u8(), (255, 0, 0, 128));
+    assert_eq!(c.to_hex_string(), "#ff000080");
+    assert_eq!(c.to_rgb_string(), "rgba(255,0,0,0.5)");
+    assert_eq!(c.to_string(), "RGBA(1,0,0,0.5)");
+
+    let c = Color::from_rgb(0., 1., 0.);
+    assert_eq!(c.to_hsva(), (120., 1., 1., 1.));
+    assert_eq!(c.to_hsla(), (120., 1., 0.5, 1.));
+    assert_eq!(c.to_hwba(), (120., 0., 0., 1.));
+
+    let c = Color::from_rgb(0., 0., 1.);
+    assert_eq!(c.to_hsva(), (240., 1., 1., 1.));
+    assert_eq!(c.to_hsla(), (240., 1., 0.5, 1.));
+    assert_eq!(c.to_hwba(), (240., 0., 0., 1.));
+
+    let c = Color::from_rgb(0., 0., 0.6);
+    assert_eq!(c.to_hsva(), (240., 1., 0.6, 1.));
+    assert_eq!(c.to_hsla(), (240., 1., 0.3, 1.));
+    assert_eq!(c.to_hwba(), (240., 0., 0.4, 1.));
+
+    let c = Color::from_rgb(0.5, 0.5, 0.5);
+    assert_eq!(c.to_hsva(), (0., 0., 0.5, 1.));
+    assert_eq!(c.to_hsla(), (0., 0., 0.5, 1.));
+    assert_eq!(c.to_hwba(), (0., 0.5, 0.5, 1.));
+}
+
+#[test]
+fn red() {
+    let data = &[
+        Color::from_rgb(1., 0., 0.),
+        Color::from_rgba(1., 0., 0., 1.),
+        Color::from_rgb_u8(255, 0, 0),
+        Color::from_rgba_u8(255, 0, 0, 255),
+        Color::from_hsv(0., 1., 1.),
+        Color::from_hsl(360., 1., 0.5),
+        Color::from_hwb(0., 0., 0.),
+        Color::from_html("red").unwrap(),
+        Color::from_html("#f00").unwrap(),
+        Color::from_html("hsv(360,100%,100%)").unwrap(),
+    ];
+    for c in data {
+        assert_eq!(c.rgba_u8(), (255, 0, 0, 255));
+    }
+}
+
+#[test]
+fn interpolate() {
+    let a = Color::from_rgb(0., 1., 0.);
+    let b = Color::from_rgb(0., 0., 1.);
+
+    assert_eq!(a.interpolate_rgb(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
+    assert_eq!(a.interpolate_rgb(&b, 0.5).rgba_u8(), (0, 128, 128, 255));
+    assert_eq!(a.interpolate_rgb(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
+
+    assert_eq!(b.interpolate_rgb(&a, 0.0).rgba_u8(), (0, 0, 255, 255));
+    assert_eq!(b.interpolate_rgb(&a, 0.5).rgba_u8(), (0, 128, 128, 255));
+    assert_eq!(b.interpolate_rgb(&a, 1.0).rgba_u8(), (0, 255, 0, 255));
+
+    assert_eq!(a.interpolate_lrgb(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
+    assert_eq!(a.interpolate_lrgb(&b, 0.5).rgba_u8(), (0, 180, 180, 255));
+    assert_eq!(a.interpolate_lrgb(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
+
+    assert_eq!(a.interpolate_hsv(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
+    assert_eq!(a.interpolate_hsv(&b, 0.5).rgba_u8(), (0, 255, 255, 255));
+    assert_eq!(a.interpolate_hsv(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,81 +1,7 @@
-extern crate csscolorparser;
-
 use csscolorparser::{parse, Color};
 
 #[test]
-fn test_color() {
-    let c = Color::from_rgb(1., 0., 0.);
-    assert_eq!(c.rgba(), (1., 0., 0., 1.));
-    assert_eq!(c.rgba_u8(), (255, 0, 0, 255));
-    assert_eq!(c.to_hex_string(), "#ff0000");
-    assert_eq!(c.to_rgb_string(), "rgb(255,0,0)");
-    assert_eq!(c.to_string(), "RGBA(1,0,0,1)");
-    assert_eq!(c.to_hsva(), (0., 1., 1., 1.));
-    assert_eq!(c.to_hsla(), (0., 1., 0.5, 1.));
-    assert_eq!(c.to_hwba(), (0., 0., 0., 1.));
-    assert_eq!(c.red(), 1.);
-    assert_eq!(c.green(), 0.);
-    assert_eq!(c.blue(), 0.);
-    assert_eq!(c.alpha(), 1.);
-
-    let c = Color::from_rgba(1., 0., 0., 0.5);
-    assert_eq!(c.rgba(), (1., 0., 0., 0.5));
-    assert_eq!(c.rgba_u8(), (255, 0, 0, 128));
-    assert_eq!(c.to_hex_string(), "#ff000080");
-    assert_eq!(c.to_rgb_string(), "rgba(255,0,0,0.5)");
-    assert_eq!(c.to_string(), "RGBA(1,0,0,0.5)");
-
-    let c = Color::from_rgb(0., 1., 0.);
-    assert_eq!(c.to_hsva(), (120., 1., 1., 1.));
-    assert_eq!(c.to_hsla(), (120., 1., 0.5, 1.));
-    assert_eq!(c.to_hwba(), (120., 0., 0., 1.));
-
-    let c = Color::from_rgb(0.5, 0.5, 0.5);
-    assert_eq!(c.to_hsva(), (0., 0., 0.5, 1.));
-    assert_eq!(c.to_hsla(), (0., 0., 0.5, 1.));
-    assert_eq!(c.to_hwba(), (0., 0.5, 0.5, 1.));
-
-    let data = vec![
-        Color::from_rgb(1., 0., 0.),
-        Color::from_rgba(1., 0., 0., 1.),
-        Color::from_rgb_u8(255, 0, 0),
-        Color::from_rgba_u8(255, 0, 0, 255),
-        Color::from_hsv(0., 1., 1.),
-        Color::from_hsl(360., 1., 0.5),
-        Color::from_hwb(0., 0., 0.),
-        Color::from_html("red").unwrap(),
-        Color::from_html("#f00").unwrap(),
-        Color::from_html("hsv(360,100%,100%)").unwrap(),
-    ];
-    for c in data {
-        assert_eq!(c.rgba_u8(), (255, 0, 0, 255));
-    }
-
-    let a = Color::from_rgb(1., 1., 1.);
-    let b = Color::from_rgb(0., 0., 0.);
-
-    assert_eq!(a.interpolate_rgb(&b, 0.0).rgba_u8(), (255, 255, 255, 255));
-    assert_eq!(a.interpolate_rgb(&b, 0.5).rgba_u8(), (128, 128, 128, 255));
-    assert_eq!(a.interpolate_rgb(&b, 1.0).rgba_u8(), (0, 0, 0, 255));
-
-    assert_eq!(b.interpolate_rgb(&a, 0.0).rgba_u8(), (0, 0, 0, 255));
-    assert_eq!(b.interpolate_rgb(&a, 0.5).rgba_u8(), (128, 128, 128, 255));
-    assert_eq!(b.interpolate_rgb(&a, 1.0).rgba_u8(), (255, 255, 255, 255));
-
-    assert_eq!(a.interpolate_lrgb(&b, 0.0).rgba_u8(), (255, 255, 255, 255));
-    assert_eq!(a.interpolate_lrgb(&b, 0.5).rgba_u8(), (180, 180, 180, 255));
-    assert_eq!(a.interpolate_lrgb(&b, 1.0).rgba_u8(), (0, 0, 0, 255));
-
-    let a = Color::from_rgb(0., 1., 0.);
-    let b = Color::from_rgb(0., 0., 1.);
-
-    assert_eq!(a.interpolate_hsv(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
-    assert_eq!(a.interpolate_hsv(&b, 0.5).rgba_u8(), (0, 255, 255, 255));
-    assert_eq!(a.interpolate_hsv(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
-}
-
-#[test]
-fn test_parser() {
+fn parser() {
     let test_data = vec![
         ("transparent", (0, 0, 0, 0)),
         ("rebeccapurple", (102, 51, 153, 255)),
@@ -100,7 +26,7 @@ fn test_parser() {
 }
 
 #[test]
-fn test_named_colors() {
+fn named_colors() {
     let test_data = vec![
         ("aliceblue", "#f0f8ff"),
         ("bisque", "#ffe4c4"),
@@ -127,7 +53,7 @@ fn test_named_colors() {
 }
 
 #[test]
-fn test_black() {
+fn black() {
     let data = vec![
         "black",
         "#000",
@@ -150,7 +76,7 @@ fn test_black() {
 }
 
 #[test]
-fn test_red() {
+fn red() {
     let data = vec![
         "red",
         "#f00",
@@ -179,7 +105,7 @@ fn test_red() {
 }
 
 #[test]
-fn test_lime() {
+fn lime() {
     let data = vec![
         "lime",
         "#0f0",
@@ -210,7 +136,7 @@ fn test_lime() {
 }
 
 #[test]
-fn test_lime_alpha() {
+fn lime_alpha() {
     let data = vec![
         "#00ff0080",
         "rgb(0,255,0,50%)",
@@ -230,7 +156,7 @@ fn test_lime_alpha() {
 }
 
 #[test]
-fn test_invalid_format() {
+fn invalid_format() {
     let test_data = vec![
         "",
         "bloodred",
@@ -256,7 +182,6 @@ fn test_invalid_format() {
         let c = parse(s);
         assert!(c.is_err());
     }
-
     let test_data = vec![
         ("#78afzd", "Invalid hex format."),
         ("rgb(255,0)", "Invalid rgb format."),

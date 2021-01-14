2944137da01bcdf04d1e206c9918dbe0eed1803b
Nor Khasyatillah <mazznoer@ymail.com>
2021-01-14 16:28:23 +0700

    Add LRGB and Oklab tests.

--- tests/color.rs
+++ tests/color.rs
@@ -11,6 +11,8 @@ fn basic() {
     assert_eq!(c.to_hsva(), (0., 1., 1., 1.));
     assert_eq!(c.to_hsla(), (0., 1., 0.5, 1.));
     assert_eq!(c.to_hwba(), (0., 0., 0., 1.));
+    assert_eq!(c.to_lrgba(), (1., 0., 0., 1.));
+    assert_eq!(c.to_oklaba(), (0.6279151939969809, 0.2249032308661071, 0.12580287012451802, 1.));
     assert_eq!(c.red(), 1.);
     assert_eq!(c.green(), 0.);
     assert_eq!(c.blue(), 0.);
@@ -51,9 +53,11 @@ fn red() {
         Color::from_rgba(1., 0., 0., 1.),
         Color::from_rgb_u8(255, 0, 0),
         Color::from_rgba_u8(255, 0, 0, 255),
+        Color::from_lrgb(1., 0., 0.),
         Color::from_hsv(0., 1., 1.),
         Color::from_hsl(360., 1., 0.5),
         Color::from_hwb(0., 0., 0.),
+        Color::from_oklab(0.6279151939969809, 0.2249032308661071, 0.12580287012451802),
         Color::from_html("red").unwrap(),
         Color::from_html("#f00").unwrap(),
         Color::from_html("hsv(360,100%,100%)").unwrap(),
@@ -83,4 +87,8 @@ fn interpolate() {
     assert_eq!(a.interpolate_hsv(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
     assert_eq!(a.interpolate_hsv(&b, 0.5).rgba_u8(), (0, 255, 255, 255));
     assert_eq!(a.interpolate_hsv(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
+
+    assert_eq!(a.interpolate_oklab(&b, 0.0).rgba_u8(), (0, 255, 0, 255));
+    assert_eq!(a.interpolate_oklab(&b, 0.5).rgba_u8(), (0, 170, 191, 255));
+    assert_eq!(a.interpolate_oklab(&b, 1.0).rgba_u8(), (0, 0, 255, 255));
 }

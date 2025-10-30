243846e37b9b91c7ccbf772b4c66a9af1ea04f18
Nor Khasyatillah <mazznoer@ymail.com>
2025-10-30 18:15:10 +0700

    fix tests

--- tests/parser_relative_color.rs
+++ tests/parser_relative_color.rs
@@ -3,10 +3,10 @@ use csscolorparser::parse;
 #[test]
 fn parser() {
     let test_data = [
-        ("rgb(from red r g b)", "#ff0000"),
-        ("rgb(from red r 127 b)", "#ff7f00"),
+        ("rgb(from #f00 r g b)", "#ff0000"),
+        ("rgb(from #f00 r 127 b)", "#ff7f00"),
         ("rgb(FROM #abcdef g B r / Alpha)", "#cdefab"),
-        ("rgb(from blue r calc(g + 90) calc(b / 2))", "#005a80"),
+        ("rgb(from #00f r calc(g + 90) calc(b / 2))", "#005a80"),
         (
             "rgb(from rgb(from #bad455 g calc(b + 23) r / alpha) b r calc(g - 23))",
             "#bad455",
